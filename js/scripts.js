80b81aac85037a10ab27e255873a7d16709e9fda
Krzysiek <kejgi@op.pl>
2017-08-25 23:40:42 +0200

    wersja alpha 0.2

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
     var self = this;
 
     this.id = randomString();
-    this.name = name;
+    this.name = name || 'Kolumna bez nazwy';
     this.$element = createColumn();
 
     function createColumn() {
@@ -52,30 +52,32 @@ $(document).ready(function(){
   function Card(description) {
     var self = this;
 
-    Card.prototype = {
-      removeCard: function() {
-        this.$element.remove();
-      }
-    };
-
     this.id = randomString();
-    this.description = description;
-    this.$element = createCard(); //
+    this.description = description || 'Karta bez nazwy';
+    this.$element = createCard();
+
 
     function createCard() {
       var $card = $('<li>').addClass('card');
       var $cardDescription = $('<p>').addClass('card-description').text(self.description);
       var $cardDelete = $('<button>').addClass('btn-delete-small button').text('X');
 
+
       $cardDelete.click(function(){
         self.removeCard();
       });
 
       $card.append($cardDelete)
-            .append($cardDescription);
-        return $card;
+      .append($cardDescription);
+      return $card;
     }
   }
+
+  Card.prototype = {
+    removeCard: function() {
+      this.$element.remove();
+    }
+  };
 
   var board = {
     name: 'Tablica Kanban',
@@ -96,7 +98,7 @@ $(document).ready(function(){
   $('.create-column').click(function(){
     var name = prompt('Podaj nazwę kolumny');
     var column = new Column(name);
-        board.addColumn(column);
+    board.addColumn(column);
   });
 
   // TWORZENIE KOLUMN
